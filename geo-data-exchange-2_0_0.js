a7d235ad711aa98bb342c2d1fb8d930e14dbbf6b
alexcojocaru <alexcojocaru@users.noreply.github.com>
2023-11-17 09:42:05 -0800

    Use the newer geo-data-exchange library

--- geo-data-exchange-2_0_0.js
+++ geo-data-exchange-2_0_0.js
@@ -8,27 +8,6 @@
     exports.internal = internal;
 
     var defaultOptions = {
-        // the track will be broken up into this many segments,
-        // for the purpose of normalizing the elevation across each segment;
-        // a high segment count, with each segment long enough, is the best choice;
-        // too short segments results in poor normalization, due to inacurate altitudes for
-        // points close to each other;
-        // too few segments results in poor gradient,
-        // due to ignoring the altitude on points in the middle;
-        // min value is 10
-        segments: 200,
-
-        // the minimum distance (in meters) between points which we consider
-        // for the purpose of calculating altitudes and gradients;
-        // this comes into play for short routes, in order to make sure
-        // we still have enough of a distance for each segment to normalize over;
-        // for long routes, the segment distance will be longer than this,
-        // thus increasing the accuracy;
-        // too short of a segment would result in poor normalization,
-        // for there won't be enough points on each segment to get a good altitude aproximation;
-        // min value is 50
-        minSegmentDistance: 200,
-
         // whether or not to compensate for the bug in Leaflet.Heightgraph
         // which makes the chart rendering to break if the track contains points
         // without an elevation/altitude coordinate;
@@ -72,59 +51,18 @@
      *       }
      *   }
      *
-     * @param {LatLng[]} latLngs - an array of LatLng objects, guaranteed to be not empty
+     * @param {LatLng[]} latLngs - an array of LatLng objects, guaranteed not to be empty
      * @param options - a set of options for building the GeoJSON feature collection;
      *     it defaults to defaultOptions if not provided
      */
     function buildGeojsonFeatures(latLngs, options) {
         var _options = typeof(options) === 'undefined' ? defaultOptions : options;
 
-        var segmentsCount = _options.segmentsCount || defaultOptions.segments;
-        if (segmentsCount < 10) {
-            segmentsCount = 10;
-        }
-
-        var minSegmentDistance = _options.minSegmentDistance || defaultOptions.minSegmentDistance;
-        if (minSegmentDistance < 50) {
-            minSegmentDistance = 50;
-        }
-
         var interpolate = typeof(_options.interpolateElevation) === 'undefined'
                 ? defaultOptions.interpolateElevation
                 : _options.interpolateElevation;
 
-        // since the altitude coordinate on geo points is not very reliable, let's normalize it
-        // by taking into account only the altitude on points at a given min distance
-        var totalDistance = _calculateDistance(latLngs);
-        var bufferMinDistance = Math.max(totalDistance / segmentsCount, minSegmentDistance);
-
-        var segments = _partitionByMinDistance(latLngs, bufferMinDistance);
-
-        var features = [];
-
-        // this is going to be initialized in the first loop, no need to initialize now
-        var currentFeature;
-
-        // undefined is fine, as it will be different to the current gradient in the first loop
-        var previousGradient;
-
-        segments.forEach(function(segment) {
-            var currentGradient = _calculateGradient(segment);
-
-            if (currentGradient == previousGradient) {
-                // the gradient hasn't changed, we can append this segment to the last feature;
-                // since the segment contains, at index 0,
-                // the last point on the current feature, add only points from index 1 onward
-                _addPointsToFeature(currentFeature, segment.slice(1), interpolate);
-            } else {
-                // the gradient has changed; create a new feature
-                currentFeature = _buildFeature(segment, currentGradient, interpolate);
-                features.push(currentFeature);
-            }
-
-            // reset to prepare for the next iteration
-            previousGradient = currentGradient;
-        });
+        var features = _buildFeatures(latLngs, interpolate);
 
         return [
             {
@@ -141,70 +79,117 @@
     exports.buildGeojsonFeatures = buildGeojsonFeatures;
 
     /**
-     * Given the list of latLng points, partition them into segments
-     * at least _minDistance__ meters long,
-     * where the first and last points on each segment always have a valid altitude.
-     * NOTE: Given that some of the given points might not have a valid altitude,
-     *       the first point(s) in the first buffer, as well as the last point(s)
-     *       in the last buffer, might not have a valid altitude.
+     * Convert the list of LatLng points to a list of elevation features.
+     *
+     * @param {LatLng[]} latLngs - an array of LatLng objects, guaranteed not to be null
+     * @param Boolean interpolate - whether to interpolate the altitude on points without it
      */
-    function _partitionByMinDistance(latLngs, minDistance) {
-        var segments = [];
+    function _buildFeatures(latLngs, interpolate) {
+        var features = [];
 
-        // temporary buffer where we add points
-        // until the distance between them is at least minDistance
-        var buffer = [];
+        if (latLngs.length === 0) {
+            return features;
+        }
 
-        // push all points up to (and including) the first one with a valid altitude
-        var index = 0;
-        for (; index < latLngs.length; index++) {
-            var latLng = latLngs[index];
-            buffer.push(latLng);
-            if (typeof latLng.alt !== "undefined") {
-                break;
+        var latLngAlts = _filterCoordinatesWithAltitude(latLngs);
+
+        if (latLngAlts.length < 2) {
+            features.push(_buildFeature(latLngs, _calculateGradient([]), interpolate));
+            return features;
+        }
+
+        // make a feature with all points without altitude at the start of the list
+        if (latLngAlts[0].index > 0) {
+            var points = latLngs.slice(0, latLngAlts[0].index + 1); // include this point
+            features.push(_buildFeature(points, _calculateGradient([]), interpolate));
+        }
+
+        var previousGradient = _calculateGradient(latLngAlts.slice(0, 2).map((lla) => lla.point));
+        var startIndex = 0;
+        for (var i = 2; i < latLngAlts.length; i++) {
+            var gradient = _calculateGradient(latLngAlts.slice(i-1, i+1).map((lla) => lla.point));
+
+            if (previousGradient != gradient) {
+                var startOfFeature = latLngAlts[startIndex];
+                var endOfFeature = latLngAlts[i-1];
+                var points = latLngs.slice(startOfFeature.index, endOfFeature.index + 1);
+                features.push(_buildFeature(points, previousGradient, interpolate));
+
+                previousGradient = gradient;
+                startIndex = i - 1;
             }
         }
 
-        // since the segments are used for gradient calculation (hence alt is needed),
-        // consider 0 length so far;
-        // that's because all points so far, except for the last one, don't have an altitude
-        var bufferDistance = 0;
+        var lastLatLngAlt = latLngAlts.slice(-1)[0];
 
-        // since index was already used, start at the next one
-        for (index = index + 1; index < latLngs.length; index++) {
-            var latLng = latLngs[index];
-            buffer.push(latLng); // the buffer contains at least 2 points by now
-            bufferDistance =
-                bufferDistance +
-                // never negative
-                buffer[buffer.length - 1].distanceTo(buffer[buffer.length - 2]);
+        // make a new feature until the last point with altitude
+        // (because the trailing points without altitude need a separate feature with 0 gradient)
+        var points = latLngs.slice(latLngAlts[startIndex].index, lastLatLngAlt.index + 1);
+        features.push(_buildFeature(points, previousGradient, interpolate));
 
-            // if we reached the tipping point, add the buffer to segments, then flush it;
-            // if this point doesn't have a valid alt, continue to the next one
-            if (bufferDistance >= minDistance && typeof latLng.alt !== "undefined") {
-                segments.push(buffer);
-                // re-init the buffer with the last point from the previous buffer
-                buffer = [buffer[buffer.length - 1]];
-                bufferDistance = 0;
-            }
+        // make a new feature with the trailing points which don't have an altitude
+        if (lastLatLngAlt.index < latLngs.length - 1) {
+            var trailingPoints = latLngs.slice(lastLatLngAlt.index, latLngs.length);
+            features.push(_buildFeature(trailingPoints, _calculateGradient([]), interpolate));
         }
 
-        // if the buffer is not empty, add all points from it (except for the first one)
-        // to the last segment
-        if (buffer.length > 0) {
-            if (segments.length === 0) {
-                segments.push(buffer);
-            } else {
-                var lastSegment = segments[segments.length - 1];
-                for (var i = 1; i < buffer.length; i++) {
-                    lastSegment.push(buffer[i]);
+        return features;
+    }
+    internal._buildFeatures = _buildFeatures;
+
+    /**
+     * Return a new array with only the points with altitude,
+     * mapped to their index in the initial array.
+     * The points without altitude, and the points which fall within fuzzy range of each other,
+     * are not included.
+     */
+    function _filterCoordinatesWithAltitude(latLngs) {
+        var latLngAlts = [];
+
+        for (var i = 0; i < latLngs.length; i++) {
+            var point = latLngs[i];
+            if (_hasAltitude(point) == false) {
+                continue;
+            }
+
+            if (latLngAlts.length > 0) {
+                var last = latLngAlts.slice(-1)[0];
+                if (_isInFuzzyRange(last.point, point)) {
+                    continue;
                 }
             }
+
+            latLngAlts.push({
+                point: point,
+                index: i
+            });
         }
 
-        return segments;
-    };
-    internal._partitionByMinDistance = _partitionByMinDistance;
+        return latLngAlts;
+    }
+    internal._filterCoordinatesWithAltitude = _filterCoordinatesWithAltitude;
+
+    /**
+     * Return true if the given point has an altitude coordinate, false otherwise.
+     */
+    function _hasAltitude(point) {
+        return typeof point.alt !== "undefined";
+    }
+    internal._hasAltitude = _hasAltitude;
+
+    /**
+     * Return true if the second point is within fuzzy range of the first point.
+     *
+     * https://www2.jpl.nasa.gov/srtm/
+     * https://wiki.openstreetmap.org/wiki/SRTM
+     *
+     * @param LatLng reference - the reference point
+     * @param LatLng point - the point to test for fuzzy range
+     */
+    function _isInFuzzyRange(reference, point) {
+        return _calculateDistance([reference, point]) < 30;
+    }
+    internal._isInFuzzyRange = _isInFuzzyRange;
 
     /**
      * Calculate the distance between all LatLng points in the given array.
@@ -231,7 +216,7 @@
         // find the index of the first point with a valid altitude
         var firstIndex = -1;
         for (var i = 0; i < latLngs.length; i++) {
-            if (typeof latLngs[i].alt !== "undefined") {
+            if (_hasAltitude(latLngs[i])) {
                 firstIndex = i;
                 break;
             }
@@ -244,7 +229,7 @@
         // find the index of the last point with a valid altitude
         var lastIndex = -1;
         for (var i = latLngs.length - 1; i > firstIndex; i--) {
-            if (typeof latLngs[i].alt !== "undefined") {
+            if (_hasAltitude(latLngs[i])) {
                 lastIndex = i;
                 break;
             }
@@ -268,29 +253,13 @@
     };
     internal._calculateGradient = _calculateGradient;
 
-    /**
-     * Add the given array of LatLng points to the end of the provided feature.
-     */
-    function _addPointsToFeature(feature, latLngs, interpolate) {
-        var latLngsWithElevation = _interpolateElevation(latLngs, interpolate);
-        latLngsWithElevation.forEach(function(point) {
-            var coordinate = [point.lng, point.lat, point.alt];
-            feature.geometry.coordinates.push(coordinate);
-        });
-    };
-
     function _buildFeature(latLngs, gradient, interpolate) {
         var latLngsWithElevation = _interpolateElevation(latLngs, interpolate);
-        var coordinates = [];
-        latLngsWithElevation.forEach(function(latLng) {
-            coordinates.push([latLng.lng, latLng.lat, latLng.alt]);
-        });
-
         return {
             type: "Feature",
             geometry: {
                 type: "LineString",
-                coordinates: coordinates
+                coordinates: latLngsWithElevation.map((p) => [p.lng, p.lat, p.alt])
             },
             properties: {
                 attributeType: gradient
@@ -320,7 +289,7 @@
         // find points without elevation and set it
         var previousIndex = -1; // keep track of the index of the previous point with elevation
         for (var i = 0; i < result.length; i++) {
-            if (typeof result[i].alt !== "undefined") {
+            if (_hasAltitude(result[i])) {
                 previousIndex = i;
                 continue;
             }
@@ -336,7 +305,7 @@
             // look up the next point with ele
             var nextIndex = i + 1;
             for (; nextIndex < result.length; nextIndex++) {
-                if (typeof result[nextIndex].alt !== "undefined") {
+                if (_hasAltitude(result[nextIndex])) {
                     break;
                 }
             }
@@ -349,7 +318,8 @@
             // fix the elevation on all points in the current series
             _interpolateElevationSublist(result, previousIndex + 1, nextIndex - 1);
 
-            // finally, since we've fixed the current series, skip to the next point
+            // finally, since we've fixed the current series,
+            // skip to the next point with elevation
             i = nextIndex - 1;
         }
 
@@ -366,14 +336,14 @@
      */
     function _interpolateElevationStart(latLngs) {
         // if the list is empty, or the first point has elevation, there's nothing to do
-        if (latLngs.length == 0 || typeof latLngs[0].alt !== "undefined") {
+        if (latLngs.length == 0 || _hasAltitude(latLngs[0])) {
             return;
         }
 
         var index = 0; // the index of the first point with elevation
         var alt = 0; // the elevation of the first point with such coordinate
         for (; index < latLngs.length; index++) {
-            if (typeof latLngs[index].alt !== "undefined") {
+            if (_hasAltitude(latLngs[index])) {
                 alt = latLngs[index].alt;
                 break;
             }
@@ -394,14 +364,14 @@
      */
     function _interpolateElevationEnd(latLngs) {
         // if the list is empty, or the last point has elevation, there's nothing to do
-        if (latLngs.length == 0 || typeof latLngs[latLngs.length - 1].alt !== "undefined") {
+        if (latLngs.length == 0 || _hasAltitude(latLngs[latLngs.length - 1])) {
             return;
         }
 
         var index = latLngs.length - 1; // the index of the last point with elevation
         var alt = 0; // the elevation of the last point with such coordinate
         for (; index >= 0; index--) {
-            if (typeof latLngs[index].alt !== "undefined") {
+            if (_hasAltitude(latLngs[index])) {
                 alt = latLngs[index].alt;
                 break;
             }
